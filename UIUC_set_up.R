c35b348b8da5dba75c4171acd17b235c3682e7fb
Elliott <elliottjmarston@gmail.com>
2021-05-23 21:32:34 -0700

    amended wd

---
+++ UIUC_set_up.R
@@ -0,0 +1,19 @@
+#### UIUC WORKSHOP ON APPLIED QUANTITATIVE GENETICS FOR PLANT BREEDERS ####
+
+### DEPENDENCIES ----
+
+# set wd
+
+setwd("C:/Users/Elliott/R/UIUC.wkshp/UIUC.wkshp.2021")
+
+# libraries
+
+library(lme4)
+library(ggplot2)
+library(rrBLUP)
+library(qqman)
+library(arm)
+library(MASS)
+library(BiocManager)
+library(multtest) # not available for current version of Rstudio
+library(gplots)
